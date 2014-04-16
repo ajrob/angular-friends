467e6a94438e928762fe059d5e1a71dd0ab34370
Alex Robinson <ajrob.dev@gmail.com>
2014-04-15 21:09:54 -0600

    Inital commit. Completes steps 1-6

--- FriendController.js
+++ FriendController.js
@@ -1,5 +1,11 @@
 
-
+app.controller('friendController', function($scope, $http){
+	$scope.test = "Hello World";
+	$http.get("https://s3.amazonaws.com/intuiplan_company_files/production/files/public/FriendData.json").
+		success(function(jsonData){
+			$scope.friends = jsonData.results;
+		});
+});
 
 
 
