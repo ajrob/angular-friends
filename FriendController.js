88bfa86a79b51bc7989046a597a7a684bdb651a2
Alex Robinson <ajrob.dev@gmail.com>
2014-04-16 21:00:03 -0600

    Finishes half of black diamond

--- FriendController.js
+++ FriendController.js
@@ -5,7 +5,42 @@ app.controller('friendController', function($scope, $http){
 		success(function(jsonData){
 			$scope.friends = jsonData.results;
 		});
+	$scope.attributeSort = "name";
+	$scope.alphabetizedSort = "false";
+	$scope.attributes = [
+		{label:'Name', value:'name'},
+		{label:'# friends', value:'friend_count'},
+		{label:'City',  value:'current_location.city'},
+		{label:'State', value:'current_location.state'},
+		{label:'Country', value:'current_location.country'}
+	];
 });
 
 
+/*
+var profilePicsDiv = document.getElementById('profile_pics');
+FB.getLoginStatus(function(response) {
+  if (response.status != 'connected') {
+    profilePicsDiv.innerHTML = '<em>You are not connected</em>';
+    return;
+  }
 
+  FB.api({ method: 'friends.get' }, function(result) {
+    Log.info('friends.get response', result);
+    var markup = '';
+    var numFriends = result ? Math.min(5, result.length) : 0;
+    if (numFriends > 0) {
+      for (var i=0; i<numFriends; i++) {
+        markup += (
+          '<fb:profile-pic size="square" ' +
+                          'uid="' + result[i] + '" ' +
+                          'facebook-logo="true"' +
+          '></fb:profile-pic>'
+        );
+      }
+    }
+    profilePicsDiv.innerHTML = markup;
+    FB.XFBML.parse(profilePicsDiv);
+  });
+});
+*/
